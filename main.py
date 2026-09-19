a2dcbfb2e4e6264b4d85e771679fce375f4ad4b8
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-09-20 02:09:00 +0530

    delete user

--- main.py
+++ main.py
@@ -29,3 +29,27 @@ async def register_user(user: User):
     db.append(user)
     return {"id: ":user.id}
 
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id==user_id:
+            db.remove(user)
+            return user_id;
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist"
+    )
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id==user_id:
+            user.first_name=user_update.first_name
+            user.last_name=user_update.last_name
+            user.middle_name=user_update.middle_name
+            user.roles=user_update.roles
+            return user_id
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists"
+    )
