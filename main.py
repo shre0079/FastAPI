48963b6a5d329425625379c022faf0ae7c036565
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-09-20 01:27:33 +0530

    fetch users

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from typing import List
+from uuid import uuid4
+
 from fastapi import FastAPI
+
+from models import Gender, Role, User
 
 app=FastAPI()
 
 db: List[User]=[
-    User(id=uuid4(),first_name="Shreyash", last_name="Mishra", gender=Gender.male, roles=[Role.student]),
-    User(id=uuid4(),first_name="Shreyash", last_name="", gender=Gender.male, roles=[Role.admin, Role.user, Role.student])
+    User(id=uuid4(), first_name="Shreyash", last_name="Mishra", gender=Gender.male, role=Role.student),
+    User(id=uuid4(), first_name="Narendra", last_name="Modi", gender=Gender.male, role=Role.admin)
 ]
 
 @app.get("/")
@@ -14,3 +19,7 @@ async def root():
 @app.get("/ping")
 async def root():
     return {"pong"} 
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;  
