9de8aae0842cdd75510afb56583d8df4b7ef6b3a
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-09-20 01:02:44 +0530

    setup local db

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
+db: List[User]=[
+    User(id=uuid4(),first_name="Shreyash", last_name="Mishra", gender=Gender.male, roles=[Role.student]),
+    User(id=uuid4(),first_name="Shreyash", last_name="", gender=Gender.male, roles=[Role.admin, Role.user, Role.student])
+]
+
 @app.get("/")
 async def root():
     return {"Hello":"Shreyash"}
